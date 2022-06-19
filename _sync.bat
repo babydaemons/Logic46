c237248b955db66eec28d6064d83c611021f6c99
MORI Shingo <babydaemons@gmail.com>
2022-06-19 21:09:47 +0900

    Logic46-13.mq4/Logic46-13.mq5：週末のエントリー禁止を実装

--- _sync.bat
+++ _sync.bat
@@ -1,5 +1,6 @@
 @echo off
 echo MQL4Å®MQL5
-xcopy /d /y Logic46.mq4 Logic46.mq5
+xcopy /d /y Logic46*.mq4 Logic46*.mq5
 echo MQL5Å®MQL4
-xcopy /d /y Logic46.mq5 Logic46.mq4
+xcopy /d /y Logic46*.mq5 Logic46*.mq4
+pause
