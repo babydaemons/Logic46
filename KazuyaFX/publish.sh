3800e9011ef770d743169f49ea57a37d854b223e
MORI Shingo <babydaemons@gmail.com>
2025-03-23 09:06:13 +0900

    ビルドスクリプト修正

--- KazuyaFX/publish.sh
+++ KazuyaFX/publish.sh
@@ -1,11 +1,9 @@
 #!/bin/bash
-WORK="_work_KazuyaFX_InstallerBuild"
+WORK="__WORK__"
 rm -rf ${WORK} KazuyaFX_Setup/KazuyaFX_Setup.zip
 mkdir ${WORK}
 
-printf "##### .NETアプリ1をビルド...\n"
-(pushd KazuyaFX_Service; dotnet publish KazuyaFX_Service.csproj -c Release -o ../${WORK}; cd ../${WORK}; rm -rf app; popd)
-printf "\n\n##### .NETアプリ2をビルド...\n"
+printf "\n##### .NETアプリをビルド...\n"
 (pushd KazuyaFX_Server; dotnet publish KazuyaFX_Server.csproj -c Release -o ../${WORK}/server; cd ../${WORK}/server; rm -rf app; popd)
 printf "\n\n##### インストーラーをビルド...\n"
 (pushd KazuyaFX_Installer; powershell -NoProfile -ExecutionPolicy Bypass -File build.ps1; cp -p KazuyaFX_Installer.exe ../${WORK}; popd)
@@ -15,7 +13,7 @@ export PATH="/c/Program Files/7-Zip:${PATH}"
 (pushd ${WORK}; find * -name \*.pdb -delete -print; 7z.exe a ../KazuyaFX_Setup/KazuyaFX_Setup.zip .; popd)
 rm -rf ${WORK}
 
-printf "\n\n##### PuInstallerでインストーラー作成...\n"
+printf "\n\n##### PyInstallerでインストーラー作成...\n"
 cd KazuyaFX_Setup
 if [ ! -f venv/Scripts/activate ]
 then
