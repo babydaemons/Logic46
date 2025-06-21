af2c54df7c46c76c64dd4d90e7836eae7c71411c
Shingo MORI <babydaemons@gmail.com>
2025-06-21 14:04:51 +0900

    タブページ・ユーザーコントロール追加

--- KazuyaFX/Teacher/KazuyaFX_StudentSetup/Form1.Designer.cs
+++ KazuyaFX/Teacher/KazuyaFX_StudentSetup/Form1.Designer.cs
@@ -29,20 +29,69 @@
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.studentSettingsControl1 = new KazuyaFX_StudentSetup.StudentSettingsControl();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tabControl1.Font = new System.Drawing.Font("Meiryo UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.tabControl1.Location = new System.Drawing.Point(0, 0);
+            this.tabControl1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(434, 251);
+            this.tabControl1.TabIndex = 0;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.studentSettingsControl1);
+            this.tabPage1.Location = new System.Drawing.Point(4, 26);
+            this.tabPage1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.tabPage1.Size = new System.Drawing.Size(426, 221);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "FXTF MT4";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // studentSettingsControl1
+            // 
+            this.studentSettingsControl1.Font = new System.Drawing.Font("Meiryo UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.studentSettingsControl1.Location = new System.Drawing.Point(3, 3);
+            this.studentSettingsControl1.Name = "studentSettingsControl1";
+            this.studentSettingsControl1.Size = new System.Drawing.Size(420, 219);
+            this.studentSettingsControl1.TabIndex = 0;
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 18F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 17F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(434, 251);
+            this.Controls.Add(this.tabControl1);
+            this.Font = new System.Drawing.Font("Meiryo UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MaximumSize = new System.Drawing.Size(450, 290);
+            this.MinimizeBox = false;
+            this.MinimumSize = new System.Drawing.Size(450, 290);
             this.Name = "Form1";
             this.Text = "生徒さんセットアップ";
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private StudentSettingsControl studentSettingsControl1;
     }
 }
---
+++ KazuyaFX/Teacher/KazuyaFX_StudentSetup/StudentSettingsControl.Designer.cs
@@ -0,0 +1,89 @@
+﻿namespace KazuyaFX_StudentSetup
+{
+    partial class StudentSettingsControl
+    {
+        private System.ComponentModel.IContainer components = null;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.CheckBox checkBoxReset;
+        private System.Windows.Forms.Button buttonSave;
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null)) components.Dispose();
+            base.Dispose(disposing);
+        }
+
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.ColumnStudentName = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.ColumnLotMultiplier = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.checkBoxReset = new System.Windows.Forms.CheckBox();
+            this.buttonSave = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.ColumnStudentName,
+            this.ColumnLotMultiplier});
+            this.dataGridView1.Location = new System.Drawing.Point(10, 10);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowTemplate.Height = 25;
+            this.dataGridView1.Size = new System.Drawing.Size(400, 140);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // ColumnStudentName
+            // 
+            this.ColumnStudentName.HeaderText = "生徒名";
+            this.ColumnStudentName.Name = "ColumnStudentName";
+            this.ColumnStudentName.Width = 200;
+            // 
+            // ColumnLotMultiplier
+            // 
+            this.ColumnLotMultiplier.HeaderText = "ロット倍率";
+            this.ColumnLotMultiplier.Name = "ColumnLotMultiplier";
+            this.ColumnLotMultiplier.Width = 150;
+            // 
+            // checkBoxReset
+            // 
+            this.checkBoxReset.AutoSize = true;
+            this.checkBoxReset.Location = new System.Drawing.Point(10, 156);
+            this.checkBoxReset.Name = "checkBoxReset";
+            this.checkBoxReset.Size = new System.Drawing.Size(350, 21);
+            this.checkBoxReset.TabIndex = 1;
+            this.checkBoxReset.Text = "Web APIのURLを設定【MT4ログイン情報は初期化されます】";
+            this.checkBoxReset.UseVisualStyleBackColor = true;
+            // 
+            // buttonSave
+            // 
+            this.buttonSave.Location = new System.Drawing.Point(10, 183);
+            this.buttonSave.Name = "buttonSave";
+            this.buttonSave.Size = new System.Drawing.Size(75, 23);
+            this.buttonSave.TabIndex = 2;
+            this.buttonSave.Text = "保存";
+            this.buttonSave.UseVisualStyleBackColor = true;
+            this.buttonSave.Click += new System.EventHandler(this.buttonSave_Click);
+            // 
+            // StudentSettingsControl
+            // 
+            this.Controls.Add(this.buttonSave);
+            this.Controls.Add(this.checkBoxReset);
+            this.Controls.Add(this.dataGridView1);
+            this.Font = new System.Drawing.Font("Meiryo UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.Name = "StudentSettingsControl";
+            this.Size = new System.Drawing.Size(420, 219);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        private System.Windows.Forms.DataGridViewTextBoxColumn ColumnStudentName;
+        private System.Windows.Forms.DataGridViewTextBoxColumn ColumnLotMultiplier;
+    }
+}
